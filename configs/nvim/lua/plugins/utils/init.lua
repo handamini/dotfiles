438e5360ea8a0873be7de0e7fdf3d70b60e78d13
handamini <handamin0130@gmail.com>
2024-07-30 02:18:17 +0900

    delete: nvim markdown-preview plugin

--- configs/nvim/lua/plugins/utils/init.lua
+++ configs/nvim/lua/plugins/utils/init.lua
@@ -1,10 +1,8 @@
 -- Utils
 local comment = require("plugins.utils.comment")
-local markdown_preview = require("plugins.utils.markdown-preview")
 local nvim_autopairs = require("plugins.utils.nvim-autopairs")
 
 return {
     comment,
-    markdown_preview,
     nvim_autopairs,
 }
